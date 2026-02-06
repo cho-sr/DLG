d69b918024afd683624b9383f08e3806f0e44b70
조석래 <cho020218@gmail.com>
2026-02-06 13:00:01 +0900

    Delete DLG directory

--- DLG/cifar-10.py
+++
@@ -1,6 +0,0 @@
-from torchvision import datasets, transforms
-
-tfm = transforms.ToTensor()
-datasets.CIFAR10(root="./data", train=True, download=True, transform=tfm)
-datasets.CIFAR10(root="./data", train=False, download=True, transform=tfm)
-print("done")
